d36410e058b7ce9d2f7ec5923702a95ea777b1bd
Antoine Théologien <antoine.theologien@etudiant.univ-reims.fr>
2024-12-17 16:43:39 +0100

    Premier_rendu + exo3

--- Premier_rendu/LRC_donneesProjet1.pl
+++ Premier_rendu/LRC_donneesProjet1.pl
@@ -35,12 +35,5 @@ subsS(C,C,_).
 subsS(C,D,_):-subs(C,D),C\==D.
 subsS(C,D,L):-subs(C,E),not(member(E,L)),subsS(E,D,[E|L]),E\==D.
 
-subsS(C,and(D1,D2),L):-D1\=D2,subsS(C,D1,L),subsS(C,D2,L).
-subsS(C,D,L):-subs(and(D1,D2),D),E=and(D1,D2),not(member(E,L)), subsS(C,E,[E|L]),E\==C.
-subsS(and(C,C),D,L):-nonvar(C),subsS(C,D,[C|L]).
-subsS(and(C1,C2),D,L):-C1\=C2,subsS(C1,D,[C1|L]).
-subsS(and(C1,C2),D,L):-C1\=C2,subsS(C2,D,[C2|L]).
-subsS(and(C1,C2),D,L):-subs(C1,E1),E=and(E1,C2),not(member(E,L)),subsS(E,D,[E|L]),E\==D.
-subsS(and(C1,C2),D,L):-Cinv=and(C2,C1),not(member(Cinv,L)),subsS(Cinv,D,[Cinv|L]).
 
 
