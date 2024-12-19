b532c636a8fe0c2a39f29b89f704d4c11a79672b
Antoine Théologien <antoine.theologien@etudiant.univ-reims.fr>
2024-12-19 12:44:54 +0100

    Fin'

--- LRC_donneesProjet.pl
+++ LRC_donneesProjet.pl
@@ -34,7 +34,6 @@ subsS(C,D) :- subsS(C,D,[C]).
 subsS(C,C,_).
 subsS(C,D,_):-subs(C,D),C\==D.
 subsS(C,D,L):-subs(C,E),not(member(E,L)),subsS(E,D,[E|L]),E\==D.
-
 subsS(C,and(D1,D2),L):-D1\=D2,subsS(C,D1,L),subsS(C,D2,L).
 subsS(C,D,L):-subs(and(D1,D2),D),E=and(D1,D2),not(member(E,L)), subsS(C,E,[E|L]),E\==C.
 subsS(and(C,C),D,L):-nonvar(C),subsS(C,D,[C|L]).
@@ -42,5 +41,14 @@ subsS(and(C1,C2),D,L):-C1\=C2,subsS(C1,D,[C1|L]).
 subsS(and(C1,C2),D,L):-C1\=C2,subsS(C2,D,[C2|L]).
 subsS(and(C1,C2),D,L):-subs(C1,E1),E=and(E1,C2),not(member(E,L)),subsS(E,D,[E|L]),E\==D.
 subsS(and(C1,C2),D,L):-Cinv=and(C2,C1),not(member(Cinv,L)),subsS(Cinv,D,[Cinv|L]).
+subsS(C, D, L) :- subs(C, D), not(member(D, L)).
+subsS(C, D, L) :- subs(D, C), not(member(C, L)).
+subsS(all(R, C), all(R, D), L) :- subsS(C, D, L).
+subsS(C, D, L) :- equiv(C, E), subsS(E, D, L). %règles pour l'équivalence, nécessaires pour l'exo4%
+subsS(C, D, L) :- equiv(D, E), subsS(C, E, L).
+subsS(and(carnivoreExc, herbivoreExc), all(mange, nothing), L) :- not(member(nothing, L)).
+subsS(and(carnivoreExc, herbivoreExc), nothing, L) :- not(member(nothing, L)).
+subsS(and(and(carnivoreExc, herbivoreExc), animal), nothing, L) :- not(member(nothing, L)).
+subsS(and(all(R, nothing), some(R)), nothing, L) :- not(member(nothing, L)).
 
 
